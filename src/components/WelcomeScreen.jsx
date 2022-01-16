90e7de3827bf309fc76c5f3d48a33f394ef95836
Artmade Studio <mouktarart@gmail.com>
2022-01-16 19:48:56 +0300

    Update WelcomeScreen.jsx

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -13,7 +13,6 @@ const WelcomeScreen = ({ levels }) => {
             game
           </div>
         </div>
-        <p className="text-white">Challenge for margot_the_</p>
         {levels.map((level, i) => (
           <a
             href={`/level/${level}`}
